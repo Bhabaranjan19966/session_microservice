63b4c867a8c15fc28f4ddf09fc7cc5a81fc932fe
bhabaranjan19966 <bhabaranjanpanigrahi@gmail.com>
2018-12-18 11:14:59 +0530

    mongo db connection added

--- server.js
+++ server.js
@@ -1,9 +1,34 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT||8080;
+const MongoClient = require('mongodb').MongoClient;
+const db;
+const couner=0;
+
+MongoClient.connect('mongo://27017',(err,client)=>{
+    if(err){console.log(err);}
+     else{
+       db=client.connect('test-db');
+       console.log('connected to data base');
+    }     
+})
+
 
 app.get('/getdata', (req,res) => {
     res.send("data is being received");
+    res.redirect('/data')
+})
+
+app.post("/data", (req , res) => {
+    db.collection('quotes').save({coun: counter},(err, result) => {
+            if(err){
+                console.log(err);
+            }
+            else{
+                counter++;
+                res.send('saved to data base');
+            }
+    })
 })
 
 app.listen(port,()=>{
